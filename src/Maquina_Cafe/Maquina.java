c3906cfcf440860eba916a9db7b1c96506c4060f
msceada1 <195111856+msceada1@users.noreply.github.com.>
2025-11-17 21:17:28 +0100

    maquina de cafe

--- src/Maquina_Cafe/Maquina.java
+++ src/Maquina_Cafe/Maquina.java
@@ -6,7 +6,7 @@ public class Maquina {
     private static final int CAPACIDAD_DEPOSITO_CAFE = 50;
     private static final int CAPACIDAD_DEPOSITO_LECHE = 50;
     private static final int CAPACIDAD_DEPOSITO_VASOS = 80;
-    private static final int PRECIO_CAFE_SOLO = 1;
+    private static final double PRECIO_CAFE_SOLO = 1;
     private static final double PRECIO_LECHE_SOLA = 0.8;
     private static final double PRECIO_CAFE_CON_LECHE = 1.5;
 
@@ -35,30 +35,80 @@ public class Maquina {
         return vasosMaquina;
     }
 
-    /**
-     * Metodo que comprueba si el dinero introducido es menor que el saldo disponible en la maquina
-     *
-     * @param dineroIntroducido el saldo que introduce el cliente
-     * @return {@code true} si el dinero del usuario es menor que el de la maquina, {@code false} en caso contrario
-     */
-    private boolean hayCambioDisponible(int dineroIntroducido) {
+    private boolean hayCambioDisponible(double dineroIntroducido) {
         return dineroIntroducido < this.saldoMaquina;
     }
 
-    private double realizarCambio(int dineroIntroducido) {
+    private double devolverCambio(double dineroIntroducido, double precioProducto) {
 
         if (!hayCambioDisponible(dineroIntroducido)) {
-            System.out.println("No tengo cambio para esa cantidad");
-            return 0.0;
+            System.out.println("No se dispone de cambio para la cantidad introducida");
+            return 0.00;
         }
 
-        System.out.println("Su cambio : ");
-        return this.saldoMaquina - dineroIntroducido;
+        this.saldoMaquina += dineroIntroducido;
+        this.saldoMaquina -= dineroIntroducido - precioProducto;
+        System.out.println("Recoja su cambio : " + (this.saldoMaquina - precioProducto));
+        return dineroIntroducido - precioProducto;
     }
 
-    public void servirCafeSolo(int dineroIntroducido) {
+    public void servirCafeSolo(double dineroIntroducido) {
 
-        System.out.println("Comprobando cambio ...");
-        System.out.println(realizarCambio(dineroIntroducido));
+        if (devolverCambio(dineroIntroducido, PRECIO_CAFE_SOLO) != 0.00) {
+            System.out.println("Sirviendo café solo... Café servido con éxito");
+            devolverCambio(dineroIntroducido, PRECIO_CAFE_SOLO);
+            this.dosisDeCafe--;
+            this.vasosMaquina--;
+        }
     }
+
+    public void servirLecheSola(double dineroIntroducido) {
+
+        if (devolverCambio(dineroIntroducido, PRECIO_LECHE_SOLA) != 0.00) {
+            System.out.println("sirviendo leche sola... Leche servida con éxito");
+            devolverCambio(dineroIntroducido, PRECIO_LECHE_SOLA);
+            this.dosisDeLeche--;
+            this.vasosMaquina--;
+        }
+    }
+
+    public void servirCafeConLeche(double dineroIntroducido) {
+
+        if (devolverCambio(dineroIntroducido, PRECIO_CAFE_CON_LECHE) != 0) {
+            System.out.println("Sirviendo cafe con leche... Cafe con leche servido con exito");
+            devolverCambio(dineroIntroducido, PRECIO_CAFE_CON_LECHE);
+            this.dosisDeLeche--;
+            this.dosisDeCafe--;
+            this.vasosMaquina--;
+        }
+    }
+
+    private void llenarDepositos() {
+
+        if (this.vasosMaquina != CAPACIDAD_DEPOSITO_VASOS) {
+            int vasosQueFaltan = CAPACIDAD_DEPOSITO_VASOS - this.vasosMaquina;
+            this.vasosMaquina += vasosQueFaltan;
+            System.out.println("deposito de vasos llenados con exito");
+        } else {
+            System.out.println("el deposito de vasos esta lleno");
+        }
+
+        if (this.dosisDeCafe != CAPACIDAD_DEPOSITO_CAFE) {
+            int dosisDeCafeQueFaltan = CAPACIDAD_DEPOSITO_CAFE - this.dosisDeCafe;
+            this.dosisDeCafe += dosisDeCafeQueFaltan;
+            System.out.println("deposito de cafe llenado con exito");
+        } else {
+            System.out.println("El deposito de cafe ya esta lleno");
+        }
+
+        if (this.dosisDeLeche != CAPACIDAD_DEPOSITO_LECHE) {
+            int dosisDeLecheQueFaltan = CAPACIDAD_DEPOSITO_LECHE - this.dosisDeLeche;
+            this.dosisDeLeche += dosisDeLecheQueFaltan;
+            System.out.println("deposito de cafe llenado con exito");
+        } else {
+            System.out.println("el deposito de leche ya esta lleno");
+        }
+    }
+
+
 }
---
+++ src/Maquina_Cafe/MaquinaAPP.java
@@ -0,0 +1,53 @@
+package Maquina_Cafe;
+
+import utils.MiEntradaSalida;
+
+public class MaquinaAPP {
+
+    public static void main(String[] args) {
+
+        Maquina maquina = new Maquina();
+
+        boolean encendida = true;
+        System.out.println("Bienvenido a la cafetera");
+
+        while (encendida) {
+
+            menu();
+
+            String opcion = MiEntradaSalida.leerCadena("que accion deseas realizar : ");
+
+            switch (opcion) {
+                case "servir cafe":
+                    double dinero = MiEntradaSalida.leerDoublePositivo("Introduce tu plata");
+                    maquina.servirCafeSolo(dinero);
+                    break;
+                case "servir cafe con leche":
+                    dinero = MiEntradaSalida.leerDoublePositivo("Introduce tu plata");
+                    maquina.servirCafeConLeche(dinero);
+                    break;
+                case "servir leche sola":
+                    dinero = MiEntradaSalida.leerDoublePositivo("Introduce tu plata");
+                    maquina.servirLecheSola(dinero);
+                    break;
+                case "consultar estado de la maquina":
+                    System.out.println("Deposito de vasos : " + maquina.getVasosMaquina());
+                    System.out.println("deposito de leche : " + maquina.getDosisDeLeche());
+                    System.out.println("deposito de cafe : " + maquina.getDosisDeCafe());
+                    break;
+                case "Salir":
+                    encendida = false;
+            }
+        }
+
+        System.out.println("Gracias por su tiempo, que tenga un buen dia");
+    }
+
+    public static void menu() {
+        System.out.println("Opciones : \n -Servir Cafe \n -Servir cafe con leche \n -Servir leche sola" +
+                "\n -Ver estado de la maquina \n -Salir");
+    }
+}
+
+
+
